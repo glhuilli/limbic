72c754c9bcd4ed783e18e84abd5f87ae6a06f743
Gaston L'Huillier <glhuilli@gmail.com>
2021-02-14 17:02:43 -0800

    NLP unit tests (#21)

---
+++ test/utils/test_nlp.py
@@ -0,0 +1,24 @@
+import unittest
+
+from limbic.utils.nlp import get_negated_words
+
+
+TEST_CASES = {
+    "I don't like this sentence": ['like'],
+    "I won't hate you.": ['hate'],
+    'I have not created this.': ['created'],
+    "You won't believe me.": ['believe'],
+    'Even John could not guess.': ['guess'],
+    'it sounds like flattery but it aint no flattery': ['flattery'],
+    "no that's not quite it": [],
+    "That's not for you to worry about": [],
+    "there is not even a dog to give food to": [],
+    'This sentence with some random text.': [],
+}
+
+
+class TestNLP(unittest.TestCase):
+    def test_process_content(self):
+        for input_test, expected_output in TEST_CASES.items():
+            output = get_negated_words(input_test)
+            self.assertEqual(output, expected_output)
